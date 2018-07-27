e213956bb8c0610b247a2c8f881b2af7aa985a2a
Kyle Shifflett <kshifflett707@gmail.com>
2018-07-26 18:31:48 -0700

    Added redux boilerplate to create-react-app boilerplate for quickly getting started on the frontend

---
+++ src/App.js
@@ -0,0 +1,44 @@
+/*
+ src/App.js
+*/
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import logo from './logo.svg';
+import './App.css';
+import { simpleAction } from './actions/simpleAction'
+
+const mapStateToProps = state => ({
+  ...state
+})
+
+const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction())
+})
+
+class App extends Component {
+  simpleAction = (event) => {
+    this.props.simpleAction();
+  }
+ render() {
+  return (
+   <div className="App">
+    <header className="App-header">
+     <img src={logo} className="App-logo" alt="logo" />
+     <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <pre>
+      {
+        JSON.stringify(this.props)
+      }
+    </pre>
+    <p className="App-intro">
+     To get started, edit <code>src/App.js</code> and save to reload
+    </p>
+    <button onClick={this.simpleAction}>Test redux action</button>
+   </div>
+  );
+ }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
