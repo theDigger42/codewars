cd71030fa73b5280468a9edc7c61c204ed6a7bf2
Kyle Shifflett <kshifflett707@gmail.com>
2018-07-29 13:13:40 -0700

    Fixed styling issue that prevented the editor from flexing when window flexes.

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -2,32 +2,41 @@ import React from 'react'
 import AceEditor from 'react-ace';
 import 'brace/mode/javascript';
 import 'brace/theme/monokai';
+import styled from 'styled-components'
 
 const Editor = (props) => {
     return (
-        <AceEditor
-            mode="javascript"
-            theme="monokai"
-            onChange={props.change}
-            value={props.input}
-            name="UNIQUE_ID_OF_DIV"
-            editorProps={{$blockScrolling: true}}
-            fontSize={16}
-            cursorStart={2}
-            showPrintMargin={true}
-            showGutter={true}
-            highlightActiveLine={true}
-            height={500}
-            width={600}
-            setOptions={{
-                enableBasicAutocompletion: true,
-                enableLiveAutocompletion: true,
-                enableSnippets: true,
-                showLineNumbers: true,
-                tabSize: 2
-            }}
-        />
+        <Wrapper>
+            <AceEditor
+                mode="javascript"
+                theme="monokai"
+                onChange={props.change}
+                value={props.input}
+                name="UNIQUE_ID_OF_DIV"
+                editorProps={{$blockScrolling: true}}
+                fontSize={16}
+                cursorStart={2}
+                showPrintMargin={true}
+                showGutter={true}
+                highlightActiveLine={true}
+                width="100%"
+                height="100%"
+                setOptions={{
+                    enableBasicAutocompletion: true,
+                    enableLiveAutocompletion: true,
+                    enableSnippets: true,
+                    showLineNumbers: true,
+                    tabSize: 2
+                }}
+            />
+        </Wrapper>
     )
 }
 
 export default Editor
+
+const Wrapper = styled.div`
+    grid-column: 1;
+    grid-row: 2;
+    min-width: 450px;
+`
--- src/containers/Challenge.js
+++ src/containers/Challenge.js
@@ -20,7 +20,7 @@ export default class Challenge extends Component {
     }
 
     componentDidMount() {
-        axios.get('/randomChallenge')
+        axios.get('http://localhost:3000/randomChallenge')
             .then(res => {
                 let challenge = res.data
                 this.setState({
@@ -68,7 +68,7 @@ export default class Challenge extends Component {
                 <Navbar {...this.props}/>
                 <Body>
                     <Prompt>{this.state.title}</Prompt>
-                    <EditorWrapper><Editor input={this.state.solution} change={this.onChange}/></EditorWrapper>
+                    <Editor input={this.state.solution} change={this.onChange}/>
                     <ResultsPanel>
                         <TabContainer>
                             <Tab onClick={() => this.changeView('instructions')}>
@@ -99,10 +99,13 @@ const Body = styled.div`
   grid-row: 2 / 4;
   grid-column: 2;
   display: grid;
-  grid-template-columns: auto auto;
+  grid-template-columns: 1.5fr 1fr;
+  grid-column-gap: 20px;
+  grid-template-rows: 100px 6fr;
+  min-height: 600px;
 `
 const Prompt = styled.div`
-  grid-row: 2;
+  grid-row: 1;
   grid-column: 1 / 3;
   text-align: center;
   font-size: 45px;
@@ -110,16 +113,13 @@ const Prompt = styled.div`
   background: lightgrey;
   margin: 25px;
 `
-const EditorWrapper = styled.div`
-  grid-row: 3;
-  grid-column: 1;
-`
 const ResultsPanel = styled.div`
   grid-column: 2;
-  grid-row: 2 / 4;
+  grid-row: 1 / 4;
   background: azure;
   display: grid;
   grid-template-rows: 50px auto 40px;
+  justify-self: right;
   margin-top: 100px;
   min-width: 400px;
 `
