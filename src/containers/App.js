f918ff61159004cec6ea741bfbf7229b3a020266
Kyle Shifflett <kshifflett707@gmail.com>
2018-08-03 17:51:44 -0700

    changed back to 60 second games just for demonstrating the app

--- src/containers/App.js
+++ src/containers/App.js
@@ -21,8 +21,8 @@ export default class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      timerTillNextGame: '',
-      gameTimer: 120,
+      timerTillNextGame: 60,
+      gameTimer: 60,
       isComplete: false
     }
     this.updateTimer = this.updateTimer.bind(this)
@@ -31,7 +31,7 @@ export default class App extends Component {
   }
 
   updateTimer(date) {
-    let secondsTillNextGame = 120 - (new Date(date).getSeconds());
+    let secondsTillNextGame = 60 - (new Date(date).getSeconds());
     this.setState({ timerTillNextGame: secondsTillNextGame });
     let timer = setInterval(() => {
       secondsTillNextGame--;
@@ -52,10 +52,10 @@ export default class App extends Component {
     let gameTimer = setInterval(() => {
       this.setState({ gameTimer: secondsTillEndGame });
       secondsTillEndGame--;
-      if (secondsTillEndGame <= 0) {
+      if (secondsTillEndGame < 0) {
         clearInterval(gameTimer);
         setTimeout(() => {
-          this.setState({ gameTimer: 120 })
+          this.setState({ gameTimer: 60 })
         }, 2000)
       }
     }, 1000)
