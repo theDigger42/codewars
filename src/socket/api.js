7d7302f2edb51719c122277d98ea04942c9b49f0
Kyle Shifflett <kshifflett707@gmail.com>
2018-08-06 20:23:14 -0700

    Ready for deploy

--- src/socket/api.js
+++ src/socket/api.js
@@ -2,7 +2,7 @@ import ioclient from 'socket.io-client'
 import store from '../store/index'
 import { getPrompt } from '../actions/prompt'
 
-const socket = ioclient.connect('http://localhost:3000', { port: 3000 });
+const socket = ioclient.connect();
 // subscribe to a Socket
 // pass in callback that gets run when recieving messages
 export const subscribeToSocket = (name, cb) => {
@@ -20,7 +20,7 @@ export const sendMessage = (message) => {
 };
 
 //timer 
-const timerSocket = ioclient('http://localhost:3000/timer')
+const timerSocket = ioclient('/timer')
 
 export const subscribeToTimerSocket = (cb) => {
   timerSocket.on('date', (date) => {
@@ -32,7 +32,7 @@ export const getDateTimerSocket = () => {
   timerSocket.emit('getDate');
 }
 
-const gameSocket = ioclient('http://localhost:3000/game');
+const gameSocket = ioclient('/game');
 
 export const subscribeToGameSocket = (onGameStart, onScoreboardChange) => {
 
